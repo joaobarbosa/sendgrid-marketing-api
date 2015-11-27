8a06c6c1010425fe95272e1474dbd8deb816821d
João Barbosa <joao.ofb@gmail.com>
2015-11-27 11:29:43 -0200

    Correct typo

--- tests/test_campaigns.py
+++ tests/test_campaigns.py
@@ -6,9 +6,7 @@ class TestCampaigns():
     def test_instantiation(self, valid_wrapper):
         assert CampaignsManager(valid_wrapper)
 
-    def test_get_all_campagins(self, valid_wrapper):
-        manager = CampaignsManager(valid_wrapper)
-
-        status, data = manager.get_all_campaings()
+    def test_get_all_campaigns(self, valid_campaign_manager):
+        status, data = valid_campaign_manager.get_all_campaings()
 
         assert status == 200
